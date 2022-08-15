63fad074c74d81634859592579fe4afb5b18c934
robsloetjes <64036294+robsloetjes@users.noreply.github.com>
2022-08-15 16:22:45 +0200

    Translation and clean up

--- ledstrip.py
+++ ledstrip.py
@@ -1,115 +1,100 @@
 import config
 import math
 import neopixel
+import tsl2561
 from machine import I2C, Pin
 from time import sleep
 
 if config.luxsensor:
     i2c = I2C(sda = Pin(config.sda), scl= Pin(config.scl))
-    if config.luxsensor_type == 'TSL2561':
-        try:
-            import tsl2561
-
-            luxsensor = tsl2561.TSL2561(i2c)
-        except:
-            print('Fout in luxsensor')
-    else:
-        import bh1750
+    try:
+        luxsensor = tsl2561.TSL2561(i2c)
+    except:
+        print('Error in lux sensor')
         
 class led_strip:
     def __init__(self):
-        self.totaal_pixels = 114
-        self.ledstrip = neopixel.NeoPixel(Pin(config.ledstrip_pin), self.totaal_pixels) #ledstrip aanmaken
-        self.r_alg = int(config.kleuralgemeen[0])
-        self.g_alg = int(config.kleuralgemeen[1])
-        self.b_alg = int(config.kleuralgemeen[2])
-        self.helderheidsfactor = int(config.helderheidsfactor)
-        self.min_helderheid = int(config.min_helderheid)
-        self.max_helderheid = int(config.max_helderheid)
-        self.ledrange_oud = []
-        self.ledrange_min_oud = []
+        self.total_pixels = 114
+        self.ledstrip = neopixel.NeoPixel(Pin(config.ledstrip_pin), self.total_pixels)
+        self.r = int(config.color[0])
+        self.g = int(config.color[1])
+        self.b = int(config.color[2])
+        self.minute_led_numbers = config.minute_led_numbers
+        self.brightness_factor = int(config.brightness_factor)
+        self.min_brightness = int(config.min_brightness)
+        self.max_brightness = int(config.max_brightness)
+        self.ledrange_old = []
+        self.ledrange_minutes_old = []
     
-    def renew_colors_algemeen(self,r,g,b):
-        self.r_alg = int(r)
-        self.g_alg = int(g)
-        self.b_alg = int(b)
-        print('nieuwe blauwwaarde is '+str(self.b_alg))
+    def renew_colors(self,r,g,b):
+        self.r = int(r)
+        self.g = int(g)
+        self.b = int(b)
     
-    def reset(self): # ledstrip uitzetten
-        for i in range(0,self.totaal_pixels):
+    def reset(self): # turn led strip off
+        for i in range(0,self.total_pixels):
             self.ledstrip[i] = (0,0,0)
-        self.ledrange_oud = []
-        self.ledrange_min_oud = []
+        self.ledrange_old = []
+        self.ledrange_minutes_old = []
         self.ledstrip.write()
        
     def getlux(self):
         if config.luxsensor:
-            if config.luxsensor_type == 'TSL2561':
-                try:
-                    lux = luxsensor.read()
-                except:
-                    print('Fout in luxsensor')
-                return lux
-            elif config.luxsensor_type == 'BH1750':
-                lux = bh1750.sample(i2c)
-                return math.sqrt(lux)/2
+            try:
+                lux = luxsensor.read()
+            except:
+                print('Fout in luxsensor')
+            return lux
         else:
             return '"False"'
     
-    def program_pixel(self, ledrange, ledminuutrange): # ledstrip programmeren
-
+    def program_pixel(self, ledrange, ledminuterange): # light up leds according to the current time
         if config.luxsensor:
             try:
-                if config.luxsensor_type == 'TSL2561':
-                    lux = luxsensor.read()
-                elif config.luxsensor_type == 'BH1750':
-                    luxread = bh1750.sample(i2c)
-                    lux = math.sqrt(luxread)/2
-                helderheid = lux * self.helderheidsfactor / 100
+                lux = luxsensor.read()
+                brightness = lux * self.brightness_factor / 100
             except:
                 print('Fout in luxsensor')
-                helderheid = self.helderheidsfactor / 50
+                brightness = self.brightness_factor / 50
         else:
-            helderheid = self.helderheidsfactor / 50
+            brightness = self.brightness_factor / 50
 
-        if helderheid > self.max_helderheid/100 or helderheid > 1:
-            helderheid = self.max_helderheid/100
-        elif helderheid < self.min_helderheid/100 or helderheid < 0:
-            helderheid = self.min_helderheid/100
-            
-        r_led = math.floor(self.r_alg / max(self.r_alg,self.g_alg,self.b_alg) * helderheid * 255) # de waarden van RGB naar verhouding t.o.v. 255 brengen, 
-        g_led = math.floor(self.g_alg / max(self.r_alg,self.g_alg,self.b_alg) * helderheid * 255) # waarbij helderheid factor tussen 0 en 1
-        b_led = math.floor(self.b_alg / max(self.r_alg,self.g_alg,self.b_alg) * helderheid * 255) # en vermenigvuldigen met helderheid
+        # Brightness should remain between 0 and 1
+        if brightness > self.max_brightness/100 or brightness > 1:
+            brightness = self.max_brightness/100
+        elif brightness < self.min_brightness/100 or brightness < 0:
+            brightness = self.min_brightness/100
+        
+        # Convert RGB to range between 0 and 255 
+        r_led = math.floor(self.r / max(self.r,self.g,self.b) * brightness * 255)
+        g_led = math.floor(self.g / max(self.r,self.g,self.b) * brightness * 255)
+        b_led = math.floor(self.b / max(self.r,self.g,self.b) * brightness * 255)
 
-        for i in range(2,112):
+        # Check for each led (not the minute leds) if it should be turned on or off
+        for i in range(2,self.total_pixels-2):
             if i in ledrange:
-                # Led moet aan, dus sowieso vernieuwen voor juiste helderheid
+                # Turn led on
                 self.ledstrip[i] = (r_led,g_led,b_led)
-                #print(str(i)+' moet aan maar stond dit nog niet')
-            if i not in ledrange and i in self.ledrange_oud:
-                # Led stond aan maar moet uit
+            if i not in ledrange and i in self.ledrange_old:
+                # Turn led off
                 self.ledstrip[i] = (0,0,0)
                 #print(str(i)+' moet uit maar stond dit nog niet')
         
-        
-        for i in [0,1, 112, 113]:
-            if i in ledminuutrange:
-                # Led moet aan, dus sowieso vernieuwen voor juiste helderheid
+        # Check for each minute leds too
+        for i in self.minute_led_numbers:
+            if i in ledminuterange:
                 self.ledstrip[i] = (r_led,g_led,b_led)
-                #print(str(i)+' moet aan maar stond dit nog niet')
-            if i not in ledminuutrange and i in self.ledrange_min_oud:
-                # Led stond aan maar moet uit
+            if i not in ledminuterange and i in self.ledrange_minutes_old:
                 self.ledstrip[i] = (0,0,0)
-                #print(str(i)+' moet uit maar stond dit nog niet')
-
         
         self.ledstrip.write()
         
-        self.ledrange_oud = ledrange
-        self.ledrange_min_oud = ledminuutrange
+        self.ledrange_old = ledrange
+        self.ledrange_minutes_old = ledminuterange
 
 def loop_leds():
-    ledstrip = neopixel.NeoPixel(Pin(14), 114)
+    # Use this function if you would like to test all leds
+    ledstrip = neopixel.NeoPixel(Pin(14), self.total_pixels)
     for i in range(0,114):
         ledstrip[i] = (255,255,255)
         if i>0:
@@ -119,10 +104,3 @@ def loop_leds():
     for i in range(0,114):
         ledstrip[i] = (0,0,0)
     ledstrip.write()
-
-
-
-
-
-
-
