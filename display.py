02fd1a626c03d7dec0775040526f02f3be0156fc
robsloetjes <64036294+robsloetjes@users.noreply.github.com>
2022-08-15 15:54:53 +0200

    Translation and clean up

--- display.py
+++ display.py
@@ -1,10 +1,13 @@
-#tijd_weergeven.py
 import math
 import config
 
 class layout:
+    # The layout contains the positions of all words, starting at the top left and continue counting after each row
     def __init__(self):
+        # "HET IS" (Dutch for IT IS) always lit
         self.prefix = list(range(0,3)) + list(range(4,6))
+        
+        # Minutes from "VIJF OVER" (5 past) to "VIJF VOOR" (5 to)
         self.minutes=[[], \
             list(range(7,11)) + list(range(40,44)), \
             list(range(11,15)) + list(range(40,44)), \
@@ -17,6 +20,8 @@ class layout:
             list(range(28,33)) + list(range(44,48)), \
             list(range(11,15)) + list(range(44,48)), \
             list(range(7,11)) + list(range(44,48)) ]
+        
+        # Hours from "EEN" (one) to "TWAALF" (twelve)
         self.hours= [list(range(99,105)), \
             list(range(51,54)), \
             list(range(55,59)), \
@@ -30,58 +35,57 @@ class layout:
             list(range(91,95)), \
             list(range(96,99)), \
             list(range(99,105))]
+        
+        # "UUR" (hour) in case of the full hour
         self.full_hour= list(range(107,110))
 
-    def get_tijdsrange(self, tijduur, tijdminuut):
-        return self.prefix + self.minutes[int(tijdminuut/5)] + self.hours[tijduur%12+(0 if tijdminuut/5 < 4 else 1)] + (self.full_hour if (tijdminuut < 5) else [])
+    def get_timerange(self, timehour, timeminute):
+        return self.prefix + self.minutes[int(timeminute/5)] + self.hours[timehour%12+(0 if timeminute/5 < 4 else 1)] + (self.full_hour if (timeminute < 5) else [])
 
-class tijdsweergave:
+class Display:
     def __init__(self, ledstrip):
-        self.tijdsranges = layout()
-        self.breedte = 11
-        self.hoogte = 10
+        self.timeranges = layout()
+        self.prefix = [102,101,82,62,61]
+        self.width = 11
+        self.height = 10
         self.ledstrip = ledstrip
         
-    def tijd_weergeven(self, hour, minute):
+    def show_time(self, hour, minute):
+        # Create range of all words that need to light up
+        timeranges = self.timeranges.get_timerange(hour, minute)
         
-        tijdsranges = self.tijdsranges.get_tijdsrange(hour, minute)
-        # Range omzetten naar coordinaten en uitvoeren
-        
-        ledminuutrange = []
+        # Convert range to coordinates
+        # First list minute leds to light up
+        ledminuterange = []
         if minute%5 == 1:
-            ledminuutrange = [self.breedte*self.hoogte+3]
+            ledminuterange = [self.width*self.height+3]
         elif minute%5 == 2:
-            ledminuutrange = [self.breedte*self.hoogte+3,1]
+            ledminuterange = [self.width*self.height+3,1]
         elif minute%5 == 3:
-            ledminuutrange = [self.breedte*self.hoogte+3,1,self.breedte*self.hoogte+2]
+            ledminuterange = [self.width*self.height+3,1,self.width*self.height+2]
         elif minute%5 == 4:
-            ledminuutrange = [self.breedte*self.hoogte+3,1,self.breedte*self.hoogte+2,0]
+            ledminuterange = [self.width*self.height+3,1,self.width*self.height+2,0]
         
         ledrange = []
-        for i in tijdsranges:
-            
-            # kolom is volledige breedte minus het residu als je door breedte deelt
-            x = (self.breedte-i%self.breedte)
-            # rij is: i/breedte, afgerond naar beneden
-            y = math.floor(i/self.breedte) + 1
-            #print(letterlijst[i]+ ': '+str(i)+' is '+str(x)+','+str(y))
-            if x%2 == 1:
-                # oneven kolommen, van boven naar beneden
-                ledrange.append((x-1) * self.hoogte + y + 1)
+        for i in timeranges:
+            # Convert horizontal position number to led number in the led string
+            x = (self.width-i%self.width)
+            y = math.floor(i/self.width) + 1
+            if x%2 == 1:  # Numbering depends on even or odd columns due to wiring
+                ledrange.append((x-1) * self.height + y + 1)
             else:
-                ledrange.append((x-1) * self.hoogte + self.hoogte -y + 2)
+                ledrange.append((x-1) * self.height + self.height -y + 2)
             
-        # print(ledrange, ledminuutrange)
-        self.ledstrip.program_pixel(ledrange, ledminuutrange)
+        self.ledstrip.program_pixel(ledrange, ledminuterange)
     
     def show_prefix(self):
-        self.ledstrip.program_pixel([102,101,82,62,61], [])
+        self.ledstrip.program_pixel(self.prefix, [])
         
     def minute_leds(self):
-        self.ledstrip.program_pixel([],[self.breedte*self.hoogte+3,1,self.breedte*self.hoogte+2,0])
+        self.ledstrip.program_pixel([],[self.width*self.height+3,1,self.width*self.height+2,0])
         
     def show_prefix_and_minute_leds(self):
-        self.ledstrip.program_pixel([102,101,82,62,61], [self.breedte*self.hoogte+3,1,self.breedte*self.hoogte+2,0])
+        self.ledstrip.program_pixel(self.prefix, [self.width*self.height+3,1,self.width*self.height+2,0])
 
 
 
